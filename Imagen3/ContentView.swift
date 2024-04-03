c4d2e1d08d83098753bc049912f992644c7bc9e7
Marco <empleomarcotapia@gmail.com>
2024-04-03 11:00:15 -0700

    trabajar con imágenes 3

--- Imagen3/ContentView.swift
+++ Imagen3/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    @State private var colorChange = false
+    @State private var sizeChange = false
+    
+       var body: some View {
+           VStack{
+               ExtractedView()
+           }
+       }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExtractedView: View {
+    var body: some View {
+        HStack{
+            
+            Image(systemName: "heart.fill")
+                .font(.system(size: 200))
+                .foregroundColor(.red)
+                .shadow(color: .gray, radius: 10, x: 0, y: 10)
+            
+            
+        }
     }
 }
