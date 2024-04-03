2412f7bfe3b5d0a93f816eb4fba9f8156c0d8ea6
Marco <empleomarcotapia@gmail.com>
2024-04-03 08:05:40 -0700

    Initial Commit

---
+++ Imagen3/Imagen3App.swift
@@ -0,0 +1,17 @@
+//
+//  Imagen3App.swift
+//  Imagen3
+//
+//  Created by Marco on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Imagen3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
